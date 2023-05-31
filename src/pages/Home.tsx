2db9b93c85fd13928f0c606dc9c1c4a1b3be0680
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-05-31 10:06:58 -0300

    Added store items to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import homeStore from '../stores/homeStore';
+import { Link } from 'react-router-dom';
 
 export interface IHomeProps {
 }
 
-export default function Home (props: IHomeProps) {
-    const store : any = homeStore()
-    React.useEffect(()=>{
-        store.fetchCoins()
-    },[])
+export default function Home(props: IHomeProps) {
+  const store: any = homeStore()
+  React.useEffect(() => {
+    store.fetchCoins()
+  }, [])
   return (
     <div>
-      Home
+      {store.coins.map((coin: any) => {
+        return (
+          <div key={coin.id}>
+            <Link to={`/${coin.id}`}>
+              {coin.name}
+            </Link>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/stores/homeStore.tsx
+++ src/stores/homeStore.tsx
@@ -2,10 +2,23 @@ import { create } from 'zustand'
 import axios from 'axios'
 
 const homeStore = create((set) => ({
-    fetchCoins: async ()=>{
-        const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
-        console.log(res)
-    }
+  coins: [],
+
+  fetchCoins: async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/search/trending')
+
+    const coins = res.data.coins.map((coin:any) => {
+      return {
+        name: coin.item.name,
+        image: coin.item.large,
+        id: coin.item.id,
+        priceBtc: coin.item.price_btc
+
+      }
+    })
+
+    set({coins})
+  }
 }))
 
 export default homeStore
