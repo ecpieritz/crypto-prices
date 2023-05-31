a74f23ab5495cbfe3ebee98542320e1d6c998f7d
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-05-31 09:31:21 -0300

    Added base Project

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+);
