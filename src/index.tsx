b6aa731ba5a459d99c5124597dbcfcae2865c5e4
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-05-31 09:33:03 -0300

    Added Show page base

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Show from './pages/Show';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route index element={<Show />} />
       </Routes>
     </BrowserRouter>
 );
