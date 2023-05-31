cd941ed55d5ac62e5945206a094548b011b64375
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-05-31 12:08:18 -0300

    Added HTML datas

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -3,62 +3,22 @@ import showStore from '../stores/showStore';
 import { useParams } from 'react-router-dom';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-
 export default function Show() {
-  const store = showStore()
-  const params = useParams()
+  const store = showStore();
+  const params = useParams();
 
   React.useEffect(() => {
-    store.fetchData(params.id)
-  }, [])
+    store.fetchData(params.id);
+  }, []);
 
   return (
-    <div>
+    <>
+      <header>
+        <img src={store.dataRes.data.image.large} alt="" />
+        <h2>{store.dataRes.data.name} {store.dataRes.data.symbol}</h2>
+        <p>{store.dataRes.data.description.en}</p>
+      </header>
+
       <AreaChart
         width={500}
         height={400}
@@ -76,6 +36,10 @@ export default function Show() {
         <Tooltip />
         <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
       </AreaChart>
-    </div>
+
+    </>
+
   );
+
+
 }
--- src/stores/showStore.jsx
+++ src/stores/showStore.jsx
@@ -5,18 +5,24 @@ const showStore = create((set) => ({
   graphData: [],
 
   fetchData: async (id) => {
-    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=30`)
+    const [graphRes, dataRes] = await Promise.all([
+      axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=30`),
+      axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`)
+    ])
 
-    const graphData = res.data.prices.map(price => {
+    const graphData = graphRes.data.prices.map(price => {
       const [timestamp, pc] = price;
+      const date = new Date(timestamp).toLocaleDateString("en-us") ;
 
       return {
-        Date: timestamp,
+        Date: date,
         Price: pc,
       };
     })
 
-    set({graphData})
+    console.log(dataRes)
+
+    set({graphData, dataRes})
   }
 }))
 
